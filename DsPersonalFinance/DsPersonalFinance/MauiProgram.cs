3b42d8605e3c2ec6cfc1aed72b43ac3a1837997c
serri98 <serri98@gmail.com>
2023-08-26 20:15:42 +0200

    added nuget packages sqlite and charts

--- DsPersonalFinance/DsPersonalFinance/MauiProgram.cs
+++ DsPersonalFinance/DsPersonalFinance/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microcharts.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace DsPersonalFinance
 {
@@ -9,6 +10,7 @@ namespace DsPersonalFinance
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMicrocharts()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
