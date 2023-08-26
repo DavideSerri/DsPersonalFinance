480b0b4536f2efd4f8addec8672b3533931b1af5
serri98 <serri98@gmail.com>
2023-08-26 20:06:07 +0200

    base project

---
+++ DsPersonalFinance/DsPersonalFinance/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DsPersonalFinance
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
